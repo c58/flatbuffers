c37e87ffbea1764522c39af8a04963bb84ad8135
Artem Artemev <viiv.c58@gmail.com>
2025-10-08 17:25:56 +0500

    added new line symbol after the condition

--- src/idl_gen_ts.cpp
+++ src/idl_gen_ts.cpp
@@ -2139,7 +2139,7 @@ class TsGenerator : public BaseGenerator {
               }
             }
             code += sig_begin + type + sig_end + " {\n";
-            code += "  if (!data.length) return 0;";
+            code += "  if (!data.length) return 0;\n";
             code += "  builder.startVector(" + NumToString(elem_size);
             code += ", data.length, " + NumToString(alignment) + ");\n";
             code += "  for (let i = data.length - 1; i >= 0; i--) {\n";
